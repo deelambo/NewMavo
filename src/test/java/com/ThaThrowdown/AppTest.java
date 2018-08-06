cd932fc84d305c9f0ebe67867eac09c3fcf739ad
Lanre Ayobamidele <deelambo@yahoo.com>
2018-08-06 18:42:57 +0100

    first mavo commit

---
+++ src/test/java/com/ThaThrowdown/AppTest.java
@@ -0,0 +1,34 @@
+package com.ThaThrowdown;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+
+
+    @Test
+    public void launchBrowser(){
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.close();
+
+    }
+}
